9db21e3623dfc0f99fbbf4d904d10bf8de92c7e1
kiiwii10 <d.lebed1099@gmail.com>
2024-06-18 13:40:16 +0300

    text changes

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -5,35 +5,39 @@ const Card = styled.div`
   background: #333;
   border: 1px solid #444;
   padding: 1rem;
-  margin: 1rem;
-  text-align: left;
+  text-align: center;
   border-radius: 8px;
   box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
   transition: transform 0.3s, box-shadow 0.3s;
   cursor: pointer;
+  display: flex;
+  flex-direction: column;
 
   &:hover {
     transform: scale(1.05);
     box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
   }
 
+  &:hover h2 {
+    color: #f39c12;
+  }
+
   h2 {
     margin: 0;
     font-size: 1.5rem;
     color: #fff;
-    text-align: center;
+    transition: color 0.3s;
   }
 
   p {
     margin: 1rem 0 0;
     font-size: 1rem;
     color: #aaa;
-    text-align: center;
   }
 `;
 
-const ProjectCard = ({ project, onClick }) => (
-  <Card onClick={onClick}>
+const ProjectCard = ({ project, onClick, className }) => (
+  <Card onClick={onClick} className={className}>
     <h2>{project.title}</h2>
     <p>{project.shortDescription}</p>
   </Card>
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -21,6 +21,13 @@ const ProjectsGrid = styled.div`
   max-width: 1200px;
 `;
 
+const CenteredProjectCard = styled(ProjectCard)`
+  grid-column: span 2;
+  justify-self: center;
+  width: 50%;
+`;
+
+
 const projects = [
   {
     title: 'Machine Learning 1',
@@ -28,8 +35,11 @@ const projects = [
     description: 'Advanced methods for data analysis and the integration of statistical tools and machine learning tools for data analysis, visual representation, and the construction of classification and prediction systems. The topics covered in the course will include: prediction and linear regression, classification systems, support vector machines, PAC, resampling, model selection and regularization, decision and regression trees, cluster analysis. Learning outcomes: theoretical understanding of the various methods and the ability to apply them to real data.',
     link: 'https://github.com/Kiiwii10/ML1',
     homeworks: [
-      { title: 'Homework 1', description: 'Description of Homework 1' },
-      { title: 'Homework 2', description: 'Description of Homework 2' },
+      { title: 'Homework 1 - Knn & logistic reg', description: '' },
+      { title: 'Homework 2 - Hard & soft SVM, Perceptron', description: '' },
+      { title: 'Homework 3 - GD, SGD and Cross Validation', description: '' },
+      { title: 'Homework 4 - RLM, ROC', description: '' },
+      { title: 'Homework 5 - Decision Trees', description: '' },
     ]
   },
   {
@@ -38,8 +48,9 @@ const projects = [
     description: 'advanced topics in machine learning that describe contemporary architectures of deep learning, emphasizing the ability to fit the training sample while generalizing, and learning that generates examples from a distribution.',
     link: 'https://github.com/Kiiwii10/ML2',
     homeworks: [
-      { title: 'Homework 1', description: 'Description of Homework 1' },
-      { title: 'Homework 2', description: 'Description of Homework 2' },
+      { title: 'Homework 1 - CNN', description: 'Implementation of CNNs and mathematical questions regarding Cross-Entropy' },
+      { title: 'Homework 2 - Sentiment Analysis', description: 'Demonstration of Fully Connected NN Overfitting and implemintation of a Sentiment Analysis Classification model' },
+      { title: 'Homework 3 - GAN', description: 'Implementation of a GAN model' },
     ]
   },
   {
@@ -48,8 +59,9 @@ const projects = [
     description: 'systematic search methods; heuristic search methods directed by an evaluation function (heuristic search), A*, construction of heuristics; planning with stochastic actions: Markov decision processes; learning from feedback, Q-learning; planning in adversarial games, alpha-beta pruning, MCTS, UCT; logical inference, combining syntax and semantics, model theories, reduction from planning to a logical problem; proof theories, resolution as a proof theory for propositional calculus; constraint satisfaction problems (CSP), trees, and tree width.',
     link: 'https://github.com/Kiiwii10/AI_fundamentals',
     homeworks: [
-      { title: 'Homework 1', description: 'Description of Homework 1' },
-      { title: 'Homework 2', description: 'Description of Homework 2' },
+      { title: 'Homework 1 - Search', description: '' },
+      { title: 'Homework 2 - MDP', description: '' },
+      { title: 'Homework 3 - MCTS', description: '' },
     ]
   },
   {
@@ -58,8 +70,9 @@ const projects = [
     description: 'Basic topics in relational database management, including design using ERD and normalization, SQL queries, and an introduction to transaction management. hands-on experience in developing and building a database.',
     link: 'https://github.com/Kiiwii10/Databases',
     homeworks: [
-      { title: 'Homework 1', description: 'Description of Homework 1' },
-      { title: 'Homework 2', description: 'Description of Homework 2' },
+      { title: 'Homework 1 - SQL, DD:', description: '' },
+      { title: 'Homework 3 - django', description: '' },
+      { title: 'Project', description: 'Simple django webpage with SQL implementation' },
     ]
   },
   {
@@ -68,8 +81,11 @@ const projects = [
     description: 'Comparative approach to object-oriented programming in C++, C#, Java, and Squeak (Smalltalk)',
     link: 'https://github.com/Kiiwii10/OOP/tree/main',
     homeworks: [
-      { title: 'Homework 1', description: 'Description of Homework 1' },
-      { title: 'Homework 2', description: 'Description of Homework 2' },
+      { title: 'Homework 1 - Squeak Basics', description: '' },
+      { title: 'Homework 2 - Java Basics', description: '' },
+      { title: 'Homework 3 - Squeak Advanced', description: '' },
+      { title: 'Homework 2 - Java Advanced', description: '' },
+      { title: 'Homework 2 - C++ metaprogramming', description: '' },
     ]
   },
   {
@@ -78,8 +94,8 @@ const projects = [
     description: 'Types of operating systems, hierarchical presentation. The concept of a process: data structures, creation, control (coordination) and destruction, inter-process communication, context switching. Main memory management: organization and implementation. Interrupt handling: input-output, scheduling, software interrupts. Real-time clock management: organization and implementation. File systems: disk input-output, data structures, directories. Input-output management: terminals, disks, communication bus, system initialization, and configuration management.',
     link: 'https://github.com/Kiiwii10/OS',
     homeworks: [
-      { title: 'Homework 1', description: 'Description of Homework 1' },
-      { title: 'Homework 2', description: 'Description of Homework 2' },
+      { title: 'Homework 1 - Smash', description: 'Implementation of a small Shell in C++' },
+      { title: 'Homework 2 - Multithreading', description: 'Implementation of a bank system using Multithreading in C++' },
     ]
   },
   {
@@ -88,25 +104,42 @@ const projects = [
     description: 'memory management, function pointers, modules, implementation of data structures (linked lists, stack, search trees), object-oriented programming (classes, templates, inheritance, polymorphism), STL and learning the C++ language. UNIX environment, tools and mechanisms, file system, processes, programming in BASH language. Software development and maintenance methodology.',
     link: 'https://github.com/Kiiwii10/Software_Structures',
     homeworks: [
-      { title: 'Homework 1', description: 'Description of Homework 1' },
-      { title: 'Homework 2', description: 'Description of Homework 2' },
+      { title: 'Homework 1 - Bash', description: '' },
+      { title: 'Homework 2 - Files', description: '' },
+      { title: 'Homework 3 - ADT, Makefile', description: '' },
+      { title: 'Homework 4 - ADT', description: '' },
+      { title: 'Homework 6 - C++', description: '' },
+      { title: 'Homework 9 - Assembly', description: '' },
     ]
   },
+  
 ];
 
 const Projects = () => {
   const [selectedProject, setSelectedProject] = useState(null);
+  const isOdd = projects.length % 2 !== 0;
 
   return (
     <ProjectsContainer id="projects">
       <ProjectsGrid>
-        {projects.map((project, index) => (
-          <ProjectCard
-            key={index}
-            project={project}
-            onClick={() => setSelectedProject(project)}
-          />
-        ))}
+        {projects.map((project, index) => {
+          if (isOdd && index === projects.length - 1) {
+            return (
+              <CenteredProjectCard
+                key={index}
+                project={project}
+                onClick={() => setSelectedProject(project)}
+              />
+            );
+          }
+          return (
+            <ProjectCard
+              key={index}
+              project={project}
+              onClick={() => setSelectedProject(project)}
+            />
+          );
+        })}
       </ProjectsGrid>
       {selectedProject && (
         <Overlay
